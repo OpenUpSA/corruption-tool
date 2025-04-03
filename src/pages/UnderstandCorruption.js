77277e984eb3a82313215299da62bf5f7071412c
desafinadude <meerkdir@gmail.com>
2025-04-03 08:59:50 +0200

    added blog v1

--- src/pages/UnderstandCorruption.js
+++ src/pages/UnderstandCorruption.js
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from 'react';
+import { React, use, useEffect, useState } from 'react';
 import { useAppContext } from '../AppContext';
 
 import { Container, Row, Col } from 'react-bootstrap';
@@ -7,11 +7,12 @@ import { Container, Row, Col } from 'react-bootstrap';
 function UnderstandCorruption() {
 
     const { koboEndpoint } = useAppContext();
+    const [posts, setPosts] = useState([]);
 
     const getPosts = async () => {
         const response = await fetch(`${koboEndpoint}/assets/amvq455NyzA54THNne6k3a/data`);
         const data = await response.json();
-        console.log(data);
+        setPosts(data.results);
     };
 
 
@@ -19,12 +20,31 @@ function UnderstandCorruption() {
         getPosts();
     }, []);
 
+    useEffect(() => {
+        console.log(posts);
+    }, [posts]);
+
 	return (
         <Container className="page-content-xl">
             <Row>
                 <Col>
                     <h1>Understand Corruption</h1>
                 </Col>
+            </Row>
+            <Row className="mt-5">
+                {
+                    posts.map((post, index) => (
+                        <Col key={index} xs={12} md={6} lg={4}>
+                            <div className="story-card">
+                                <div className="story-card-image" style={{backgroundImage: `url(${post._attachments[0].download_url.replace('?format=json','')})`}}></div>
+                                <div className="story-card-content">
+                                    <h2>{post.Title}</h2>
+                                    <p>{post.Excerpt}</p>
+                                </div>
+                            </div>
+                        </Col>
+                    ))
+                }
             </Row>
         </Container>
 	);
