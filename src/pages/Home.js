d75fd30210a4b84270f8f080bcdd697e5e79a45b
michaelglenister <mickeyjhg@gmail.com>
2025-06-30 11:17:34 +0200

    Direct visitors to EC province by default

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
 import { useAppContext } from '../AppContext';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 import Map from '../components/Map';
 import Panel from '../components/Panel';
@@ -9,21 +9,30 @@ import Breadcrumbs from '../components/Breadcrumbs';
 
 
 function Home() {
-
     const { choroplethCounts } = useAppContext();
+    const navigate = useNavigate();
+    const location = useLocation();
 
-	return (
-        <>  
+    useEffect(() => {
+        const params = new URLSearchParams(location.search);
+        if (!params.has('geo')) {
+            params.set('geo', 'EC');
+            navigate({ pathname: location.pathname, search: params.toString() }, { replace: true });
+        }
+    }, [location.search, navigate]);
+
+    return (
+        <>
             <div className="map-container">
                 <div style={{ height: '100%', width: '100%' }}>
                     <Map counts={choroplethCounts} />
                 </div>
                 <Panel />
-                <Breadcrumbs page="home"/>
+                <Breadcrumbs page="home" />
                 <Search />
             </div>
         </>
-	);
+    );
 }
 
 export default Home;
